b38a6095efc7f1b93287b181eaa34e0deec93819
hitak <hitak77@naver.com>
2023-12-18 10:09:29 +0900

    앱 개발 학습 (인강) Custom Adapter 생각보다 난이도 있음

--- c34/src/main/java/com/tak/c34/DriveAdapter.kt
+++ c34/src/main/java/com/tak/c34/DriveAdapter.kt
@@ -32,6 +32,10 @@ class DriveAdapter(cxt: Context, val resId: Int, val datas: MutableList<DriveVO>
         }
 
 
+
+        //===========================================================================
+        // 일단 핵심 기능 및 알고리즘은 아래의 내용임
+
         val holder = convertView.getTag() as DriveHolder
         val typeImageView = holder.typeImageView
         val titleView = holder.titleView
@@ -63,15 +67,29 @@ class DriveAdapter(cxt: Context, val resId: Int, val datas: MutableList<DriveVO>
                 )
             )
         }
-
-
+        //===========================================================================
+        // 얘 까지도 어떻게보면 지금까지의 쉽게 알 수있는 직접적인 그런 기능구현이라 해야하나 그런 위치
         menuImageView.setOnClickListener{
             Toast.makeText(context, "$title menu click", Toast.LENGTH_SHORT)
                 .show()
         }
 
+        //===========================================================================
+
         return convertView
 
 
     }
+
 }
+
+/**
+ *
+ * 솔직히 getView 초반에 inflater 쓰면서 없으면 임의로(?) 새로 만들어서 써먹는거 좀 어려워서 이해안가고
+ * 데이터 클래스랑 미리 findViewById로 잡아놓는 클래스 따로 만드는거 자체를 이해하는건 쉬운데
+ * 왜 이렇게 까지 나누는가에 대한 필요성에 대한 이해가 잘 안되긴함
+ * 근데 핵심은 그게 아니긴하니까 일단 ㅇㅋ...
+ *
+ * 일단 이런식으로 단순히 리스트 쫙 내려서 하는 리스트어댑터나 심플 어댑터 그런게 아니고서야
+ * 이런식으로 임의로 커스텀 어댑터를 만들어줘야한다는 것
+ */
